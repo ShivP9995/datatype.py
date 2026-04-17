f357c04eb457ff09f8f9d5108cb3aeb72fcdab64
ShivP9995 <shivp9995@gmail.com>
2026-04-17 10:07:05 +0530

    Create Datatype.py

---
+++ Datatype.py
@@ -0,0 +1,43 @@
+# Data Type Operations
+
+x = "hello"
+print(type(x))
+
+x = 10
+print(type(x))
+
+x = 2.50
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+x = range(6)
+print(type(x))
+
+x = {"name": "john", "age": 26}
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+
+x = True
+print(type(x))
+
+x = b"hello"
+print(type(x))
+
+x = bytearray(5)
+print(type(x))
+
+x = memoryview(bytes(5))
+print(type(x))
+
+x = None
+print(type(x))
